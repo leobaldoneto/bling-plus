a822ddb95438f8070969a414d7a5432405bf4f9b
Leobaldo Alcantara Neto <leobaldoneto@gmail.com>
2020-11-13 21:25:00 -0300

    Create auto-print-nfce-pdv.js

---
+++ auto-print-nfce-pdv.js
@@ -0,0 +1,10 @@
+// No Frente de Caixa, imprime NFC-e automaticamente após a autorização da Sefaz.
+
+// link mask:
+*bling.com.br*pdv.php
+
+// javascript:
+setInterval(function(){
+	let button = document.querySelector(".act-print-invoice");
+	button.click();
+}, 400);
